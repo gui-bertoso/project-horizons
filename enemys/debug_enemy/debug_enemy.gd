c151886fdb625e797e94779e67339d6335d3789d
gui.bertoso <154436826+gui-bertoso@users.noreply.github.com>
2026-02-23 16:20:45 -0300

    test: addictions and tests

--- enemys/debug_enemy/debug_enemy.gd
+++ enemys/debug_enemy/debug_enemy.gd
@@ -1,6 +1,8 @@
 extends EnemyTemplate
 class_name DebugEnemy
 
+@export var health: int = 9
+
 func update_state(_delta: float) -> void:
 	match current_state:
 		"":
@@ -25,4 +27,12 @@ func apply_state() -> void:
 			var direction: Vector2 = (player_reference.global_position - global_position).normalized()
 			if direction:
 				velocity = direction * move_speed
+		"death":
+			velocity.x = move_toward(velocity.x, 0.0, move_speed)
+			velocity.y = move_toward(velocity.y, 0.0, move_speed)
 			
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	health -= area.damage
+	if health <= 0:
+		current_state = "death"
